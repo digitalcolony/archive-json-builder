a2e3e2287f3ebcb34228890a68b19c198055ee71
MAS <digitalcolony@gmail.com>
2017-11-19 16:42:13 -0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# archive-json-builder
+# JSON builder for Archive.org
+
+This tool scrapes a URL on Archive.org and then builds a JSON file with all the audio file details.
+
+## Setup
+
+Find a URL on Archive.org that hosts audio files. 
+
+EXAMPLE: [https://archive.org/details/neil-rogers-show-soundboard](https://archive.org/details/neil-rogers-show-soundboard)
+
+Add that URL into the the app.js as the url variable.
---
+++ app.js
@@ -0,0 +1,78 @@
+var request = require("request"),
+  cheerio = require("cheerio"),
+  fs = require("fs"),
+  moment = require("moment"),
+  url = "https://archive.org/details/neil-rogers-show-soundboard",
+  jsonFilename = url
+    .replace("https://archive.org/details/", "")
+    .concat(".json");
+
+// Load page hosting audio from archive.org
+request(url, function(error, response, body) {
+  if (!error) {
+    var $ = cheerio.load(body);
+
+    // Build array of show names
+    var name = $('meta[itemprop="name"]')
+      .map(function() {
+        return $(this).attr("content");
+      })
+      .get();
+
+    // Build array of show duration afer converting ugly (EX: PT0M458S) to HH:MM:SS format
+    var duration = $('meta[itemprop="duration"]')
+      .map(function() {
+        var strDuration = $(this).attr("content");
+        strDuration = strDuration.replace("PT0M", "").replace("S", "");
+
+        return moment()
+          .startOf("day")
+          .seconds(strDuration)
+          .format("H:mm:ss");
+      })
+      .get();
+
+    // build array of MP3 links
+    var mp3 = $('link[itemprop="associatedMedia"]')
+      .map(function() {
+        if (
+          $(this)
+            .attr("href")
+            .endsWith(".mp3")
+        ) {
+          return $(this).attr("href");
+        }
+      })
+      .get();
+
+    // build array of OGG links
+    var ogg = $('link[itemprop="associatedMedia"]')
+      .map(function() {
+        if (
+          $(this)
+            .attr("href")
+            .endsWith(".ogg")
+        ) {
+          return $(this).attr("href");
+        }
+      })
+      .get();
+
+    // merge the 4 arrays into a single array then convert to JSON
+    var jsonArr = [];
+    for (var i = 0; i < name.length; i++) {
+      jsonArr.push({
+        name: name[i],
+        duration: duration[i],
+        mp3: mp3[i],
+        ogg: ogg[i]
+      });
+    }
+    var myJSON = JSON.stringify(jsonArr);
+
+    // save JSON file to disk
+    fs.writeFileSync(jsonFilename, JSON.stringify(jsonArr), "utf8");
+  } else {
+    console.log("We’ve encountered an error: " + error);
+  }
+});
